eec56f20b28e7829306906d01bbc89242c4bf68f
Stu Franks <stu@stuts.uk>
2022-02-28 16:30:48 +0000

    Get only powr-related events, quote variables for linux bash support

--- usage.sh
+++ usage.sh
@@ -2,8 +2,7 @@ NODES=$(openstack server list -f csv -c Name --quote none |tail -n +2)
 
 for node in $NODES ; do
 
-    USAGE=$(openstack server event list $node -f csv -c Action -c "Start Time" --quote none --sort-ascending --sort-column "Start Time")
-    USAGE_NO_HEAD=$(echo "$USAGE" |tail -n +2)
+    USAGE=$(openstack server event list $node -f csv -c Action -c "Start Time" --quote none --sort-ascending --sort-column "Start Time" |grep -E 'create|start|stop')
 
     flavor=$(openstack server show $node -f value -c flavor |sed 's/ (.*//g')
     tags=$(openstack server show $node -f yaml -c properties |grep -v properties)
@@ -26,8 +25,9 @@ for node in $NODES ; do
         echo "    tags:"
         while read t ; do
             echo "      $t"
-        done <<< $tags
+        done <<< "$tags"
 
-    done <<< $USAGE_NO_HEAD
+    done <<< "$USAGE"
 
 done
+
